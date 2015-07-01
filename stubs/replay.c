7920aa7e78222286c00a2cba8439a37faeebf547
Pavel Dovgalyuk <Pavel.Dovgaluk@ispras.ru>
2015-07-01 13:47:26 +0300

    replay: recording and replaying clock ticks
    Clock ticks are considered as the sources of non-deterministic data for
    virtual machine. This patch implements saving the clock values when they
    are acquired (virtual, host clock).
    When replaying the execution corresponding values are read from log and
    transfered to the module, which wants to read the values.
    Such a design required the clock polling to be synchronized. Sometimes
    it is not true - e.g. when timeouts for timer lists are checked. In this case
    we use a cached value of the clock, passing it to the client code.

--- stubs/replay.c
+++ stubs/replay.c
@@ -1,3 +1,12 @@
 #include "replay/replay.h"
 
 ReplayMode replay_mode;
+
+int64_t replay_save_clock(unsigned int kind, int64_t clock)
+{
+}
+
+int64_t replay_read_clock(unsigned int kind)
+{
+    return 0;
+}
