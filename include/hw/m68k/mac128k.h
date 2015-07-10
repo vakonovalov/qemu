c1f3e774a3b7cf3e814be2c01404e3509db8e058
npobka <lenchik.102@mail.ru>
2015-07-10 12:16:04 +0400

    add new adapter

--- include/hw/m68k/mac128k.h
+++ include/hw/m68k/mac128k.h
@@ -9,4 +9,7 @@ void iwm_init(MemoryRegion *sysmem, uint32_t base, M68kCPU *cpu);
 /* sy6522.c */
 void sy6522_init(MemoryRegion *sysmem, uint32_t base, M68kCPU *cpu);
 
+/* testsystem.c */
+void testsystem_init(MemoryRegion *sysmem, uint32_t base, M68kCPU *cpu);
+
 #endif
