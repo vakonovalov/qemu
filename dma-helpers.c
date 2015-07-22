bace3ee20e6c4ddf00faf403d5c7315d355dc01d
Pavel Dovgaluk <pavel.dovgaluk@ispras.ru>
2015-07-22 15:24:00 +0300

    Removed obsolete function

--- dma-helpers.c
+++ dma-helpers.c
@@ -93,15 +93,6 @@ static void reschedule_dma(void *opaque)
     dma_blk_cb(dbs, 0);
 }
 
-static void continue_after_map_failure(void *opaque)
-{
-    DMAAIOCB *dbs = (DMAAIOCB *)opaque;
-
-    dbs->bh = qemu_bh_new_replay(reschedule_dma, dbs,
-                                 replay_get_current_step());
-    qemu_bh_schedule(dbs->bh);
-}
-
 static void dma_blk_unmap(DMAAIOCB *dbs)
 {
     int i;
