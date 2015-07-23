68b9f18335fbd3127494774149bc41dea14681b1
Pavel Dovgaluk <pavel.dovgaluk@ispras.ru>
2015-07-23 11:41:22 +0300

    Add stubs for usermode compilation

--- stubs/replay.c
+++ stubs/replay.c
@@ -1,5 +1,9 @@
 #include "replay/replay.h"
 #include "sysemu/sysemu.h"
+#include "qemu/notify.h"
+#include "ui/input.h"
+#include "block/aio.h"
+#include "block/thread-pool.h"
 
 ReplayMode replay_mode;
 
@@ -23,6 +27,10 @@ int runstate_is_running(void)
     return 0;
 }
 
+void qemu_system_shutdown_request(void)
+{
+}
+
 uint64_t replay_get_current_step(void)
 {
     return 0;
@@ -38,5 +46,21 @@ void replay_add_bh_event(void *bh, uint64_t id)
 }
 
 void replay_add_thread_event(void *opaque, void *opaque2, uint64_t id)
+{
+}
+
+void qemu_input_event_send_impl(QemuConsole *src, InputEvent *evt)
+{
+}
+
+void qemu_input_event_sync_impl(void)
+{
+}
+
+void aio_bh_call(QEMUBH *bh)
+{
+}
+
+void thread_pool_work(ThreadPool *pool, void *r)
 {
 }
