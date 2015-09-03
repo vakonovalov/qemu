f0f107ca5d5bebfce85380093fe42ddedf1d392e
Pavel Dovgaluk <pavel.dovgaluk@gmail.com>
2015-09-03 19:22:32 +0300

    Add linux-user stubs

---
+++ replay/replay-user.c
@@ -0,0 +1,25 @@
+#include "replay.h"
+
+bool replay_exception(void)
+{
+    return true;
+}
+
+bool replay_has_exception(void)
+{
+    return true;
+}
+
+bool replay_interrupt(void)
+{
+    return true;
+}
+
+bool replay_has_interrupt(void)
+{
+    return true;
+}
+
+void replay_finish(void)
+{
+}
